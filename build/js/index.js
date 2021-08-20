0df4f23f9618f27da60f797b561cf8adc7603603
“Syxw” <yxw_ye@163.com>
2021-08-20 15:26:30 +0800

    优化版” git commit -m 优化版1.1”

--- build/js/index.js
+++ build/js/index.js
@@ -99,3 +99,9 @@ var scope = {
   animationLoop(scope);
 }(scope);
 
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.addEventListener('resize', function () {
+        scope.ctx.canvas.width = window.innerWidth;
+        scope.ctx.canvas.height = window.innerHeight;
+    });
+}
